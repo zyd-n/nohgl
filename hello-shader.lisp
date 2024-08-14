732399bb1f567ab1603c56869932bc628786730a
zyd <zyd-p@proton.me>
2024-08-14 02:10:42 -0600

    Reorganize hello-shader

--- hello-shader.lisp
+++ hello-shader.lisp
@@ -3,6 +3,11 @@
 (defun hello-shaders ()
   (start 'hello-shaders :title "Hello, Shaders!" :width 900 :height 600))
 
+(defvar [m] (mat4 1.0 0.0 0.0 0.0
+                  0.0 1.0 0.0 0.0
+                  0.0 0.0 1.0 0.0
+                  0.0 0.0 0.0 1.0))
+
 (defun draw-vertex (i)
   (gl:bind-buffer :array-buffer *vbo-handle*)
   (gl:enable-vertex-attrib-array i)
@@ -11,30 +16,16 @@
   (gl:disable-vertex-attrib-array i))
 
 (define-render hello-shaders
-    ((color 0.0)
-     (scale 0.0)
+    ((scale 0.0)
      (delta 0.005))
-  (gl:clear-color .09 .09 .09 0)
-  (labels ((update-vertex-position (location mtransform)
-             (gl:uniform-matrix-4fv location mtransform)
-             (setf scale (+ scale delta)
-                   delta (if (or (>= scale 1.0) (<= scale -1.0))
-                             (* delta -1.0)
-                             delta)))
-           (cycle-color (r g b &optional (a 1.0))
-             (gl:clear-color r g b a)
-             (if (>= color 1.0)
-                 (setf color 0.0)
-                 (incf color (/ 1.0 255.0))))
-           (clear-buffer ()
-             (gl:clear :color-buffer)
-             (glfw:swap-buffers *g*)))
-    (let* ((m (mat4 1.0 0.0 0.0 0.0
-                    0.0 1.0 0.0 0.0
-                    0.0 0.0 1.0 0.0
-                    0.0 0.0 0.0 1.0))
-           (a (tfrom-mat m)))
-      (update-vertex-position gscale-location (marr (tmat (tscale-by a 1.0 (* scale 0.9) 1.0))))
+  (let ((m (marr (tmat (tscale-by (tfrom-mat [m]) 1.0 (* scale 0.9) 1.0)))))
+    (labels ((update-vertex-position (location mtransform)
+               (gl:uniform-matrix-4fv location mtransform)
+               (setf scale (+ scale delta)
+                     delta (if (or (>= scale 1.0) (<= scale -1.0))
+                               (* delta -1.0)
+                               delta))))
+      (update-vertex-position gscale-location m)
       (draw-vertex 0)
       (glfw:swap-buffers *g*)
       (gl:clear :color-buffer))))
