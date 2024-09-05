e4aedf606ac042259ab1dffb6471d4a7a16bb298
zyd <zyd-p@proton.me>
2024-09-05 15:35:20 -0600

    Add delta-time to world-clock

--- source/time.lisp
+++ source/time.lisp
@@ -4,8 +4,10 @@
 
 (defclass world-clock ()
   ((duration :initform (error "Must supply a duration of some kind.") :initarg :duration :accessor duration)
-   (ticks :reader ticks :initform 0)
-   (frames :reader frames :initform 0.0)))
+   (ticks :accessor ticks :initform 0)
+   (frames :accessor frames :initform 0.0)
+   (last-frame :accessor last-frame :initform 0.0)
+   (delta-time :accessor delta-time :initform 0.0)))
 
 (defgeneric forward-time (clock))
 (defgeneric log-time (clock))
@@ -13,23 +15,40 @@
 (defun nsec (n)
   (local-time:timestamp+ (local-time:now) n :sec))
 
+;; We currently confuse two things here: the creation of our world clock and
+;; setting how often to log it.
 (defun time-by (duration)
   (make-instance 'world-clock :duration duration))
 
 (defmethod forward-time ((clock world-clock))
-  (with-slots (duration ticks frames) clock
-    (when (local-time:timestamp>= (local-time:now) duration)
-      (setf (duration clock) (nsec 1))
+  (let ((now (glfw:time)))
+    (with-accessors ((duration duration)
+                     (ticks ticks)
+                     (frames frames)
+                     (last-frame last-frame)
+                     (delta-time delta-time))
+        clock
+      (setf delta-time (- now last-frame))
+      (setf last-frame now)
       (incf ticks)
-      (log-time clock)
-      (decf frames frames))
-    (incf frames))
+      (incf frames)
+      (when (local-time:timestamp>= (local-time:now) duration)
+        (setf duration (nsec 1))
+        (log-time clock)
+        (decf frames frames))))
   clock)
+
+(defun world-clock ()
+  (clock (current-context)))
+
+(defun dt ()
+  (delta-time (world-clock)))
 
 (defmethod log-time ((clock world-clock))
   (when *world-stats*
-    (with-slots (ticks frames) clock
-      (format t "Tick: ~s~%FPS: ~s~%Miliseconds: ~s~%--------------------~%~%" ticks frames (/ 1000.0 frames)))))
+    (with-slots (ticks frames delta-time) clock
+      (format t "Tick: ~s~%FPS: ~s~%Miliseconds: ~s~%Delta: ~s~%--------------------~%~%"
+              ticks frames (/ 1000.0 frames) delta-time))))
 
 (defun debug-with-time (&optional (enable t))
   (setf *world-stats* enable))
